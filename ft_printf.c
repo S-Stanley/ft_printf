189deeacc5615d2c28f538769e53e39bb8843132
Stanley SERBIN <stanleyserbin@gmail.com>
2021-02-28 18:31:44 +0100

    ADD with 1/2

--- ft_printf.c
+++ ft_printf.c
@@ -1,24 +1,65 @@
 #include "./ft_printf.h"
 
+char		*ft_add_left(char *s, int size_min)
+{
+	int	size;
+	char	*s2;
+	int	i;
+	int	x;
+
+	i = 0;
+	x = 0;
+	size = size_min - ft_strlen(s);
+	s2 = malloc(sizeof(char) * (ft_strlen(s) + size_min + 1));
+	while (size-- > 0)
+	{
+		s2[i] = ' ';
+		i++;
+	}
+	while (s[x])
+	{
+		s2[i] = s[x];
+		i++;
+		x++;
+	}
+	s2[i] = '\0';
+	return (s2);
+}
+
 t_res		ft_proxy(char *str, t_res res, va_list data)
 {
-	char *s;
+	char 	*s;
+	int	width;
 	
 	res.i++;
-	s = get_value(str[res.i], data);
+	width = 0;
 	if (ft_is_flag(&str[res.i]))
 	{
 		printf("FLAG ");
 	}
 	if (ft_is_with(&str[res.i]))
-		printf("WITH ");
+	{
+		if (str[res.i] == '*')
+			printf("star");
+		else
+		{
+			while (str[res.i] >= '1' && str[res.i] <= '9')
+			{
+				width = (width * 10) + (str[res.i] - 48);
+				res.i++;
+			}
+		}
+	}
 	if (ft_is_prevision(&str[res.i]))
 		printf("PRECISION ");
 	if (ft_isletter(&str[res.i]))
-	{
-		res.str = ft_join(res.str, s);
-		res.i++;
-	}
+		s = get_value(str[res.i], data);
+
+
+	if (width)
+		s = ft_add_left(s, width); 
+	res.str = ft_join(res.str, s);
+	res.i++;
 	return (res);
 }
 
