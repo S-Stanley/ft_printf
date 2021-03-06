6d67e5e81caa8b4efa36faa3895ff8ecca28defb
Stanley SERBIN <stanleyserbin@gmail.com>
2021-03-06 18:43:43 +0100

    486

--- main.c
+++ main.c
@@ -1,9 +1,33 @@
 #include "ft_printf.h"
 
+	int		a = -2;
+	int		b = 0;
+	char	c = 'a';
+	int		d = 2147483647;
+	int		e = -2147483648;
+	int		f = 42;
+	int		g = 25;
+	int		h = 4200;
+	int		i = 8;
+	int		j = -12;
+	int		k = 123456789;
+	int		l = 0;
+	int		m = -12345678;
+	char	*n = "abcdefghijklmnop";
+	char	*o = "-a";
+	char	*p = "-12";
+	char	*q = "0";
+	char	*r = "%%";
+	char	*s = "-2147483648";
+	char	*t = "0x12345678";
+	char	*u = "-0";
+
 int main(void)
 {
-	// printf("%d\n", ft_printf("%0*.*i, %0*.*d, %0*.*d, %0*.*d, %0*.*d, %0*.*d, %0*.*d, %0*.*d\n", a, b, i, a, b, j, a, b, k, a, b, l, a, b, m, a, b, c, a, b, e, a, b, d));
-	printf("%d\n", ft_printf("%0*.*d\n", 3, -2, 'a'));
+	a = -2;
+	b = -2;
+	// printf("%d\n", printf("%0*.*i, %0*.*d, %0*.*d, %0*.*d, %0*.*d, %0*.*d, %0*.*d, %0*.*d\n", a, b, i, a, b, j, a, b, k, a, b, l, a, b, m, a, b, c, a, b, e, a, b, d));
+	printf("%d\n", ft_printf("%0*.*i\n", a, b, i));
 	// printf("|%d", ft_printf("%0*d", ));
 	return (0);
 }
